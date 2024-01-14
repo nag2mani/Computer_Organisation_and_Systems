df20862b0ae6eb04c7d677377b1a4cd7b80e71b0
Nagmani kumar <nag2mani@gmail.com>
2024-01-14 13:47:47 +0530

    and logic added

--- first.v
+++ first.v
@@ -1,12 +1,15 @@
-//This is not logic.
-module first(a, b); //Here a is input and b is output.
-input a;
-input b;
-assign b = ~a;
-endmodule
+// //This is for not logic.
+// module first(a, b); //Here a is input and b is output.
+// input a;
+// input b;
+// assign b = ~a;
+// endmodule
 
 
-//This is and logic.
+
+
+
+//This is for and logic.
 module first(a,b,c);
 input a,b;
 output c;
