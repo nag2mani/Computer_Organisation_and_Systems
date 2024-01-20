adcf1d7ce16bfd6a2321302fc7118de22dfe3a4f
Nagmani kumar <nag2mani@gmail.com>
2024-01-20 10:34:21 +0530

    logic done

--- first.v
+++ first.v
@@ -1,50 +1,50 @@
-// //This is for not logic.
-// module first(a, b); //Here a is input and b is output.
-// input a;
-// input b;
-// assign b = ~a;
-// endmodule
+//This is for not logic.
+module first(a, b); //Here a is input and b is output.
+input a;
+input b;
+assign b = ~a;
+endmodule
 
 
 
 
 
-// //This is for and logic.
-// module first(a, b, c);
-// input a, b;
-// output c;
-// assign c = a & b;
-// endmodule
+//This is for and logic.
+module first(a, b, c);
+input a, b;
+output c;
+assign c = a & b;
+endmodule
 
 
 
 
-// //This is for and logic.
-// module first(a, b, c);
-// input a, b;
-// output c;
-// assign c = a | b;
-// endmodule
+//This is for and logic.
+module first(a, b, c);
+input a, b;
+output c;
+assign c = a | b;
+endmodule
 
 
 
-// //This is for XOR logic.
-// module first(a, b, c);
-// input a,b;
-// output c;
-// assign c = a ^ b;
-// endmodule
+//This is for XOR logic.
+module first(a, b, c);
+input a,b;
+output c;
+assign c = a ^ b;
+endmodule
 
 
 
 
 
-// //This is for NOR logic.
-// module first(a, b, c);
-// input a,b;
-// output c;
-// assign c = ~(a | b);  // for nor logic ~(A | B)
-// endmodule
+//This is for NOR logic.
+module first(a, b, c);
+input a,b;
+output c;
+assign c = ~(a | b);  // for nor logic ~(A | B)
+endmodule
 
 
 
